376077cdc9e68cff59e30970f4e31eadb506786d
Blake A. Long <blake.a.long@gmail.com>
2019-07-25 16:15:39 -0600

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,22 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds.to_json(except:
+        [:created_at, :updated_by]) 
+        # the above is being used to explicity render in JSON and to show that except and only are actually parameters of the .to_json method.
+      # birds, only: 
+      # [:id, :name, :species]
+      # except: 
+      # [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id:params[:id])
+    if bird
+      render json: {id: bird.id, name: bird.name, species: bird.species}
+    # bird.slice(:id, :name, :species)
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 end
